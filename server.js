e78b28ad72f8c2218199af92271a0a52a758987d
Romulo Silva <romulo.silva@osf-commerce.com>
2019-06-10 22:21:11 -0300

    Refactor Spotify service

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-import Spotify from './src/Spotify';
+import Spotify from './src/services/Spotify';
 import Artist from './src/controllers/Artist';
 
 const app = express();
