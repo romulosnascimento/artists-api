4414423bdb3c8305e1db061784a2bfd3643e22b1
Romulo Silva <romulo.silva@osf-commerce.com>
2019-06-10 22:20:22 -0300

    Refactor Spotify service

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import axios from 'axios';
 import dotenv from 'dotenv';
 
+import Spotify from './src/Spotify';
 import Artist from './src/controllers/Artist';
 
 const app = express();
@@ -9,33 +9,8 @@ app.use(express.json());
 
 dotenv.config();
 
-const spotifyConfig = {};
-spotifyConfig.authorization = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64');
-
-const handleSpotifyAuthorization = (req, res, next) => {
-    if (spotifyConfig.token) {
-        req.spotifyToken = spotifyConfig.token;
-        next();
-    } else {
-        const body = 'grant_type=client_credentials';
-        const config = {
-            headers: {
-                'Authorization': `Basic ${spotifyConfig.authorization}`,
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-        axios.post('https://accounts.spotify.com/api/token', body, config)
-            .then(response => {
-                spotifyConfig.token = response.data;
-                req.spotifyToken = spotifyConfig.token;
-                next();
-            })
-            .catch(error => res.status(500).send(error));
-    }
-}
-
-app.get('/api/v1/artists', handleSpotifyAuthorization, Artist.search);
-app.get('/api/v1/artists/:id', handleSpotifyAuthorization, Artist.get);
+app.get('/api/v1/artists', Spotify.handleAuthorization, Artist.search);
+app.get('/api/v1/artists/:id', Spotify.handleAuthorization, Artist.get);
 
 app.listen(process.env.PORT, () => {
     console.log('App running on port ', process.env.PORT);
